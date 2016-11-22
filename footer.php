e43a40d39a58d5fa3efac30eff9470ee2e3dc371
Angel Angeles <angelangelesiii@gmail.com>
2016-11-22 09:33:16 +0800

    Added Underscores starter theme

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Phobos
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'phobos' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'phobos' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'phobos' ), 'phobos', '<a href="http://zimitmedia.com" rel="designer">Angel Angeles III, Almer Almazan</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
